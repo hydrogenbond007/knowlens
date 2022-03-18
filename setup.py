ec15de5e4530a302e9eb98d8f4f47e11587e5410
McCrearyD <mccreary@dyllan.ai>
2022-03-18 16:12:13 -0700

    install testing requirements + add new dir

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@ from numpy import require
 from setuptools import setup
 
 
-setup(name="knowlens", version="0.0.1", requires=open("requirements.txt").read().splitlines())
+setup(name="knowlens", version="0.0.1", requires=open("requirements/common.txt").read().splitlines())
