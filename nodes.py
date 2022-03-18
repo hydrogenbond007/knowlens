0a61e42c51f4c946eb591bd43313536bc4af1fef
McCrearyD <mccreary@dyllan.ai>
2022-03-18 15:23:36 -0700

    rename tokenize + implement basic tokenization

---
+++ nodes.py
@@ -0,0 +1,40 @@
+import os
+from collections import defaultdict
+from transformers import BertTokenizer, XLNetTokenizer
+
+# tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+tokenizer = XLNetTokenizer.from_pretrained("xlnet-base-cased")
+
+
+def preprocess(text: str) -> str:
+    # TODO: docstring
+    text = text.replace(".md", "")
+    return text.replace("_", "").replace(" ", "").replace("-", "").replace(".", "")
+
+
+def tokenize(text: str) -> str:
+    text = preprocess(text)
+    return "".join(tokenizer.tokenize(text))
+
+
+def get_token_to_paths_mapping(directories: list):
+    # TODO: docstring
+
+    token_to_paths_mapping = defaultdict(list)
+
+    for directory in directories:
+        for file_name in os.listdir(directory):
+            if not file_name.endswith(".md"):
+                raise ValueError("Right now only markdown files are supported")
+
+            # tokenize filename in case there are files with similar, but not exactly similar, names
+            token = tokenize(file_name)
+            path = os.path.join(directory, file_name)
+            token_to_paths_mapping[token].append(path)
+
+    return dict(token_to_paths_mapping)
+
+
+if __name__ == "__main__":
+    tokens_to_paths = get_token_to_paths_mapping(["./data/knowledge_base1", "./data/knowledge_base2"])
+    print(tokens_to_paths)
